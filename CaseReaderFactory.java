b0479519167d81767ca52c67b7215b375365f114
sapien2710 <81070935+sapien2710@users.noreply.github.com>
2022-07-23 14:49:25 -0700

    Added 14 java files

---
+++ CaseReaderFactory.java
@@ -0,0 +1,28 @@
+//Abhijeet Purkar
+//apurkar
+
+package hw3;
+
+public class CaseReaderFactory {
+
+	public CaseReader createReader(String filename)
+	{
+		CaseReader result = null;
+		int len = filename.length();
+		String type = filename.substring(len-3);   //Checking for the file type
+		switch(type)
+		{
+		case "csv":
+			result = new CSVCaseReader(filename);
+			break;
+		case "tsv":
+			result = new TSVCaseReader(filename);
+			break;
+		default:
+			break;	
+		}
+		return result;
+		
+	}
+
+}
---
+++ ModifyCaseView.java
@@ -0,0 +1,25 @@
+//Abhijeet Purkar
+//apurkar
+
+package hw3;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ModifyCaseView extends CaseView {
+
+	ModifyCaseView(String header) {
+		super(header);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	Stage buildView() {
+		
+		updateButton.setText("Modify case");
+		Scene sam = new Scene(updateCaseGridPane);
+		stage.setScene(sam);
+		return stage;
+		
+	}
+}
